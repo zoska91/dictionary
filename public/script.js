f5c3076058a2fdc5380b315ab16e33152c573486
zoska91 <zoska91@wp.pl>
2019-07-25 20:26:51 +0200

    add style.css, change index.html (with BEM), only for mobile phone - for now, add some function in JS in front

--- public/script.js
+++ public/script.js
@@ -1,24 +1,81 @@
-const wordsTable = document.querySelector('.words');
+//show Word
+const showBtn = document.querySelector("[data-button='btnShow']");
 
-const btnShowWords = document.querySelector('.showWords');
+const showSeconsdWord = () => {
+  console.log('ok');
+  document.querySelector('.main__second-word').classList.toggle('main__second-word--active');
+  showBtn.classList.toggle('main__button--next');
 
-const showWords = words => {
-  words.forEach(word => {
-    div = document.createElement('div');
-    wordsTable.appendChild(div);
-
-    div.textContent = `${word.englishWord} - ${word.polishWord}`;
-  });
-  console.log(words[1]);
+  if (showBtn.textContent === 'show') showBtn.textContent = 'next';
+  else showBtn.textContent = 'show';
 };
 
-btnShowWords.addEventListener('click', () => {
-  fetch('/words', {
-    method: 'GET'
-  })
-    .then(res => res.json())
-    .then(words => {
-      console.log(words);
-      showWords(words);
-    });
-});
+showBtn.addEventListener('click', showSeconsdWord);
+
+// add new Word
+
+const addNewWordBtn = document.querySelector("[data-button='btnAddCard']");
+
+let activeCardaddNewWord = false;
+const mainCard = document.querySelector('.main');
+const addWordCard = document.querySelector('.addWord');
+
+const closeMainCard = () => {
+  mainCard.style.opacity = '0';
+  setTimeout(() => {
+    mainCard.style.display = 'none';
+    addWordCard.style.display = 'block';
+    addWordCard.style.opacity = 1;
+  }, 200);
+};
+
+const closeaddWordCard = () => {
+  addWordCard.style.opacity = '0';
+  setTimeout(() => {
+    addWordCard.style.display = 'none';
+    mainCard.style.display = 'block';
+    mainCard.style.opacity = 1;
+  }, 200);
+};
+
+const showNewWordCard = () => {
+  if (!activeCardaddNewWord) {
+    closeMainCard();
+    addNewWordBtn.textContent = 'go back';
+    addNewWordBtn.style.borderColor = 'rgb(192, 15, 15)';
+  } else {
+    closeaddWordCard();
+    addNewWordBtn.textContent = 'add new';
+    addNewWordBtn.style.borderColor = 'black';
+  }
+  activeCardaddNewWord = !activeCardaddNewWord;
+};
+
+addNewWordBtn.addEventListener('click', showNewWordCard);
+document.querySelector("[data-button='btnAddWord']").addEventListener('click', showNewWordCard);
+
+// dictionary
+// const wordsTable = document.querySelector('.words');
+
+// const btnShowWords = document.querySelector('.showWords');
+
+// const showWords = words => {
+//   words.forEach(word => {
+//     div = document.createElement('div');
+//     wordsTable.appendChild(div);
+
+//     div.textContent = `${word.englishWord} - ${word.polishWord}`;
+//   });
+//   console.log(words[1]);
+// };
+
+// btnShowWords.addEventListener('click', () => {
+//   fetch('/words', {
+//     method: 'GET'
+//   })
+//     .then(res => res.json())
+//     .then(words => {
+//       console.log(words);
+//       showWords(words);
+//     });
+// });
