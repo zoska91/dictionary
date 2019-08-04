740efd11f7cba06ea12f69111ca33e901b93a158
zoska91 <zoska91@wp.pl>
2019-08-04 10:21:26 +0200

    finish ver 1.2 - change css

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 const client = new mongo.MongoClient(config.db, { useNewUrlParser: true });
 //mongodb+srv://zoska91:deskorolka1@cluster0-tttr1.mongodb.net/test?retryWrites=true&w=majority
 
-app.listen(3000, () => console.log('Server at http://localhost:3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Server at http://localhost:3000'));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  db: 'mongodb+srv://zoska91:deskorolka1@cluster0-tttr1.mongodb.net/test?retryWrites=true&w=majority',
+  keySession: ['TWOJKLUCZ'],
+  maxAgeSession: 24 * 60 * 60 * 1000 //24 h
+};
