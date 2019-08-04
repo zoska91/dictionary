d2441be382466f02afb980851ea06ba98daf5a80
zoska91 <zoska91@wp.pl>
2019-08-04 09:47:04 +0200

    finishi ver 1.0 - change db

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const path = require('path');
 const dictRoutes = require('./routes/dict');
 const mongo = require('mongodb');
+const config = require('./config');
+// const mongoose = require('mongoose');
+
+// mongoose.connect(config.db, { useNewUrlParser: true });
 
 const app = express();
-const client = new mongo.MongoClient('mongodb://localhost:27017', { useNewUrlParser: true });
+const client = new mongo.MongoClient(config.db, { useNewUrlParser: true });
+//mongodb+srv://zoska91:deskorolka1@cluster0-tttr1.mongodb.net/test?retryWrites=true&w=majority
 
 app.listen(3000, () => console.log('Server at http://localhost:3000'));
 
